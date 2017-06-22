d741ce98711bbfc93a2b9f38e29e6a630c9514df
niharsaini <nihar.saini2002@gmail.com>
2017-06-22 18:09:17 +0530

    nihars

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,20 @@ var URL ='mongodb://nihar:Nihar%4022@ds131782.mlab.com:31782/nihar';
 
 const db=require('monk')(URL);
 
-const dot =db.get('srj');
+const docs =db.get('srj');
 /* GET home page. */
 router.get('/welcome', function(req, res, next) {
 
  res.send("abmnu");
 });
 router.get('/wel', function(req, res, next) {
-    dot.find({}, function(err,docs){
-       // res.send("cmmmm")
+    docs.insert({firstname:"nihar",lastname:"saini",age:"21"}, function(err,docs){
         if(err) console.log(err);
-        else res.json(dot);
+        else res.json(docs);
     })
-
+});
+router.get('/ni',function (reg,res,next) {
+    res.send('welcome to heroku');
 
 });
 

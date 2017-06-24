e88d07eebdd5e3625659100ff5156c396bf88fc1
niharsaini <nihar.saini2002@gmail.com>
2017-06-24 16:33:47 +0530

    dfdf

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/users', users);
 
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE, PUT');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });

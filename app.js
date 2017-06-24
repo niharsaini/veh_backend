63fcda67c6095ff0c079bcd689d5fcf3f0a8c86c
niharsaini <nihar.saini2002@gmail.com>
2017-06-24 16:29:07 +0530

    ergj

--- app.js
+++ app.js
@@ -26,20 +26,13 @@ app.use('/', index);
 app.use('/users', users);
 
 
-
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8100/ionic-lab');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
-
+ 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');

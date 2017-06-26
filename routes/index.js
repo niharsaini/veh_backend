9bcf9bdd2b783a1553c88f481b4a3d42fb2805d3
nihar saini <nihar.saini2002@gmail.com>
2017-06-26 18:29:26 +0530

    tghghg
    ghghggh

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,11 @@ router.get('/push', function(req, res, next) {
     })
 });
 router.post('/insert', function(req, res, next) {
- var username=req.body.name;
- var pass=req.body.password;
+var username=req.body.name;
+var pass=req.body.password;
     docs.insert({"name":username,"password":pass}, function(err,docs){
         if(err) console.log(err);
-        else res.json(docs[0]);
+        else res.send("successful");
     })
 });
 router.get('/find', function(req, res, next) {
